22b46970441546a232871e06dcda0664e27e1ecb
Matheus Dussin Bampi <matheusbampi@gmail.com>
2016-03-22 11:22:10 -0300

    Analysis alterado, não necessitando mais os espaços.

--- pseudocode_converter/src/pseudocode_converter/Analysis.java
+++ pseudocode_converter/src/pseudocode_converter/Analysis.java
@@ -5,7 +5,7 @@
  */
 package pseudocode_converter;
 
-import java.lang.String;
+import java.util.LinkedList;
 
 /**
  *
@@ -26,22 +26,26 @@ public class Analysis {
         }
     }
 
-    public void testDeclaration(String[] line, boolean flag) throws VarDeclarationException, RWordNotFoundException {
+    /*
+    String esq e dir no ligar de String[] line
+    esq == line[0] && dir == line[2]
+     */
+    public void testDeclaration(String esq, String dir, boolean flag) throws VarDeclarationException, RWordNotFoundException {
         if (flag) {
             // Testando se as variáveis foram declaradas corretamente.
-            String[] var = line[2].split(",");
+            String[] var = dir.split("\\s*\\,\\s*");
             for (String var1 : var) {
                 if (Character.isDigit(var1.charAt(0))) {
                     throw new VarDeclarationException("Erro na declaração das variáveis.");
                 }
             }
-            if (!"inteiro".equals(line[0].toLowerCase()) && !"real".equals(line[0].toLowerCase())) {//MODIFICADO:Adicionado .toLowerCase() para suportar letras M e m
+            if (!"inteiro".equals(esq.toLowerCase()) && !"real".equals(esq.toLowerCase())) {//MODIFICADO:Adicionado .toLowerCase() para suportar letras M e m
                 throw new RWordNotFoundException("Palavra reservada incorreta.");
             }
         } else {
             int c = 0;
-            String[] var = line[2].split("\\s*\\,\\s*");//MODIFICADO: não suportava espaços antes e depois da virgula
-            String s = line[0] + ";";
+            String[] var = dir.split("\\s*\\,\\s*");//MODIFICADO: não suportava espaços antes e depois da virgula
+            String s = esq + ";";
             for (String var1 : var) {
                 c++;
                 s += var1 + ";";
@@ -50,53 +54,70 @@ public class Analysis {
         }
     }
 
-    public void testAttribution(String[] line, boolean flag) throws BadExpressionException {
+    /*
+    String esq e dir no ligar de String[] line
+    esq == line[0] && dir == line[2]
+    retirado if e else que comparava length == 1
+     */
+    public void testAttribution(String esq, String dir, boolean flag) throws BadExpressionException {
         if (flag) {
-            String exp = line[2];
-                        for(int y = 0; y<exp.length()-1; y++){
-                            // Testando repetição de operador.
-                            if(exp.charAt(y) == '+' || exp.charAt(y) == '-' || exp.charAt(y) == '*' || exp.charAt(y) == '/'){
-                                if(exp.charAt(y+1) == '+' || exp.charAt(y+1) == '-' || exp.charAt(y+1) == '*' || exp.charAt(y+1) == '/'){
-                                    throw new BadExpressionException("Erro na expressão.");
-                                }
-                            }
-                        }
-                        
+            String exp = dir;
+            for (int y = 0; y < exp.length() - 1; y++) {
+                // Testando repetição de operador.
+                if (exp.charAt(y) == '+' || exp.charAt(y) == '-' || exp.charAt(y) == '*' || exp.charAt(y) == '/') {
+                    if (exp.charAt(y + 1) == '*' || exp.charAt(y + 1) == '/') { // retirado '+' e '-' devido a n negativo e positivo
+                        throw new BadExpressionException("Erro na expressão.");
+                    }
+                }
+            }
         } else {
-           String exp = line[2];
-           String AT;
-                    if(exp.length() == 1 ){
-                       AT = "AT;"+line[0]+";"+exp;//MODIFICAOD += por =; RETIRADO \n
-                       t.addRow(AT); //MODIFICADO ADICIONADO .addrow(AT)
-                    }
-                    else {
-                       PolishNotation pn = new PolishNotation(exp);
-                       pn.toPostfix();
-                       String e = pn.toString();
-                       AT = "AT;"+line[0]+";"+e;//+= por =; RETIRADO \n
-                       t.addRow(AT);
-                    }
+            String exp = dir;
+            String AT;
+            PolishNotation pn = new PolishNotation(exp);
+            pn.toPostfix();
+            String e = pn.toString();
+            AT = "AT;" + esq + ";" + e;//+= por =; RETIRADO \n
+            t.addRow(AT);
         }
     }
 
-    public void testPrinting(String[] line) {
-
-        if (line[1].charAt(0) == '"' && line[1].charAt(line[1].length() - 1) == '"') {
-            t.addRow("OUTS;" + line[1] + ";");
-        } else {
-            t.addRow("OUTV;" + line[1] + ";");
+    public void testPrinting(String conteudo) {
+        boolean bool = true;
+        char letra;
+        int ultimaVirgula = 0;
+        LinkedList<String> l = new LinkedList();
+        String[] var;
+        for (int i = 0; i < conteudo.length(); i++) {
+            letra = conteudo.charAt(i);
+            switch (letra) {
+                case '"':
+                    bool = !bool;
+                    break;
+                case ',':
+                    if (bool) {
+                        l.add(conteudo.substring(ultimaVirgula, i).replaceAll("[^\\s+|\\s+$]", ""));//remove espacos nas extremidades
+                    }
+                    ultimaVirgula = i;
+                    break;
+            }
+        }
+        for (String l1 : l) {
+            if (l1.charAt(0) == '"') {
+                t.addRow("OUTS;" + l1 + ";");
+            } else {
+                t.addRow("OUTV;" + l1 + ";");
+            }
         }
     }
 
-    public void testReading(String[] line) {
+    public void testReading(String conteudo) {
         int i;
-        String var, saida;
+        String saida;
         saida = "IN;";
-        for (i = 1; line[i].charAt(line[i].length() - 1) == ','; i++) {
-            var = line[i].substring(0, line[i].length() - 1);
+        String[] variaveis = conteudo.split("[\\s*,\\s*]");
+        for (String var : variaveis) {
             saida += var + ";";
         }
-        saida += line[i] + ";";
         t.addRow(saida);
     }
 
@@ -105,20 +126,13 @@ public class Analysis {
     }
 
     public boolean LexicalAnalysis() throws RWordNotFoundException, VarDeclarationException, BadExpressionException {
-
-        for (String file1 : file) {
-            String[] line = file1.split(" ");
-            if (line.length == 1) {
-
-            } else if (line.length == 3) {
-                switch (line[1]) {
-                    case ":":
-                        this.testAttribution(line, true);
-                        break;
-                    case "=":
-                        this.testDeclaration(line, true);
-                        break;
-                }
+        for (String linha : file) { //MOD: String "file1" por "linha"
+            if (linha.matches(":")) {
+                String[] lado = linha.split(":", 2); // divide em 2 substring que dividem no primeiro ':' encontrado
+                this.testDeclaration(lado[0], lado[1], true);
+            } else if (linha.matches("=")) {
+                String[] lado = linha.split("=", 2); // divide em 2 substring que dividem no primeiro ':' encontrado
+                this.testAttribution(lado[0], lado[1], true);
             }
         }
         return true;
@@ -126,47 +140,25 @@ public class Analysis {
 
     public String SyntacticAnalysis() throws VarDeclarationException, RWordNotFoundException, BadExpressionException {
         t = new Table();
-        for (String file1 : file) {
-            String[] line = file1.split("\\s+");//MODIFICADO: não suportava multiplos espaços.OBS:deve ser tratado para retirar espaços duplicados            if (line.length == 3) {
-            if (":".equals(line[1])) {
-                this.testDeclaration(line, false);
-            }
-            if ("=".equals(line[1])) {
-                this.testAttribution(line, false);
-            }
-            /*if("se".equals(line[0])){
-                    for (String line1 : line) {
-                        switch(line1){
-                            case "==":
-                                String esq = "";
-                                String dir = "";
-                                for (int i = 0; line[i] != line1;i++){
-                                   esq += line[i];
-                                }
-                                for (int i = line.length; line[i] != line1; i--){
-                                   dir += line[i];
-                                }
-                                break;
-                            
-                            case "!=":
-                                break;
-                            
-                            case ">":
-                                break;
-                            
-                            case "<":
-                                break;
-                    }
-                }*/
-            if ("escreva".equals(line[0].toLowerCase())) {
-                this.testPrinting(line);
-            }
-            if ("leia".equals(line[0].toLowerCase())) {
-                this.testReading(line);
+        for (String linha : file) { //MOD: String "file1" por "linha"
+            if (":".matches(linha)) {
+                String[] lado = linha.split("\\s*:\\s*", 2); // divide em 2 substring que dividem no primeiro ':' encontrado
+                this.testDeclaration(lado[0], lado[1], false);
+            } else if ("se".matches(linha)) {
+
+            } else if ("senao".matches(linha)) {
+
+            } else if ("fimse".matches(linha)) {
+
+            } else if ("escreva".matches(linha.toLowerCase())) {
+                this.testPrinting(linha.split("escreva[\\s*(\\s*]", 2)[1].split("[\\s*);\\s*$]", 2)[0]);
+            } else if ("leia".matches(linha.toLowerCase())) {
+                this.testReading(linha.split("leia[\\s*(\\s*]", 2)[1]);
+            } else if ("=".matches(linha)) {
+                String[] lado = linha.split("=\\s*", 2); // divide em 2 substring que dividem no primeiro '=' encontrado
+                this.testAttribution(lado[0], lado[1], false);
             }
         }
         return t.toString();
     }
-
-    
 }
